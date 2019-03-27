a49b57677edc3e8621ea5fee26df53124a21b6ed
songzhikai <songzhikai@bonc.com.cn>
2019-03-27 15:59:04 +0800

    提交

--- src/views/pages/home_page.jsx
+++ src/views/pages/home_page.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import {connect} from 'dva';
 import './views_pages.css'
-import $$ from 'cmn-utils'
-import { NavBar, Icon, Badge, Tabs, WhiteSpace  } from 'antd-mobile';
-import { StickyContainer, Sticky } from 'react-sticky';
+import {Button, DatePicker, Icon, List, NavBar, SearchBar, TabBar, Tabs, WhiteSpace, WingBlank, ActivityIndicator, Steps} from 'antd-mobile';
+import {Sticky, StickyContainer} from 'react-sticky';
+
+const Step = Steps.Step;
+
+const customIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42" className="am-icon am-icon-md">
+    <g fillRule="evenodd" stroke="transparent" strokeWidth="4">
+      <path d="M21 0C9.402 0 0 9.402 0 21c0 11.6 9.402 21 21 21s21-9.4 21-21C42 9.402 32.598 0 21 0z" />
+      <path fill="#FFF" d="M29 18.73c0-.55-.447-1-1-1H23.36l4.428-5.05c.407-.46.407-1.208 0-1.668-.407-.46-1.068-.46-1.476 0l-5.21 5.89-5.21-5.89c-.406-.46-1.067-.46-1.475 0-.406.46-.406 1.207 0 1.667l4.43 5.05H14.23c-.55 0-.998.45-.998 1 0 .554.448.97 1 .97h5.9v3.942h-5.9c-.552 0-1 .448-1 1s.448.985 1 .985h5.9v4.896c0 .552.448 1 1 1 .55 0 .968-.284.968-.836v-5.06H28c.553 0 1-.433 1-.985s-.447-1-1-1h-5.9v-3.94H28c.553 0 1-.418 1-.97z" />
+    </g>
+  </svg>
+);
 
 const tabs2 = [
   { title: 'First Tab', sub: '1' },
@@ -21,41 +31,254 @@ const tabs = [
   { title: 'Second Tab' },
   { title: 'Third Tab' },
 ];
-class FlexPage extends React.Component {
+const nowTimeStamp = Date.now();
+const now = new Date(nowTimeStamp);
+const utcNow = new Date(now.getTime() + (now.getTimezoneOffset() * 60000));
 
+class FlexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+      fullScreen: false,
+      date: now,
+      time: now,
+      utcDate: utcNow,
+      dpValue: null,
+      customChildValue: null,
+      visible: false,
+      value: '美食',
+      animating: false,
+    };
+  }
+  renderContent(pageText) {
+    return (
+      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+        <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+        <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
+           onClick={(e) => {
+             e.preventDefault();
+             this.setState({
+               hidden: !this.state.hidden,
+             });
+           }}
+        >
+          Click to show/hide tab-bar
+        </a>
+        <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
+           onClick={(e) => {
+             e.preventDefault();
+             this.setState({
+               fullScreen: !this.state.fullScreen,
+             });
+           }}
+        >
+          Click to switch fullscreen
+        </a>
+      </div>
+    );
+  }
+  onChange= (value) => {
+    this.setState({ value });
+  };
+  clear = () => {
+    this.setState({ value: '' });
+  };
+  handleClick = () => {
+    this.manualFocusInst.focus();
+  }
+  showToast = () => {
+    this.setState({ animating: !this.state.animating });
+    this.closeTimer = setTimeout(() => {
+      this.setState({ animating: !this.state.animating });
+    }, 1000);
+  }
   render() {
     return (
       <div>
-        <NavBar
-          mode="dark"
-          icon={<Icon type="left" />}
-          onLeftClick={() => console.log('onLeftClick')}
-          rightContent={[
-            <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
-            <Icon key="1" type="ellipsis" />,
-          ]}
-        >NavBar</NavBar>
-
+        <div>
+          <NavBar
+            mode="dark"
+            icon={<Icon type="left" />}
+            onLeftClick={() => console.log('onLeftClick')}
+            rightContent={[
+              <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+              <Icon key="1" type="ellipsis" />,
+            ]}
+          >NavBar</NavBar>
+        </div>
         <WhiteSpace />
-        <StickyContainer>
-          <Tabs tabs={tabs}
-                initalPage={'t2'}
-                renderTabBar={renderTabBar}
+        <div>
+          <StickyContainer>
+            <Tabs tabs={tabs}
+                  initalPage={'t2'}
+                  renderTabBar={renderTabBar}
+            >
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
+                Content of first tab
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
+                Content of second tab
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
+                Content of third tab
+              </div>
+            </Tabs>
+          </StickyContainer>
+        </div>
+        <WhiteSpace />
+        <div>
+          <List className="date-picker-list" style={{ backgroundColor: 'white' }}>
+            <DatePicker
+              value={this.state.date}
+              onChange={date => this.setState({ date })}
+            >
+              <List.Item arrow="horizontal">Datetime</List.Item>
+            </DatePicker>
+          </List>
+        </div>
+        <WhiteSpace />
+        <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+          <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#33A3F4"
+            barTintColor="white"
+            hidden={this.state.hidden}
+            tabBarPosition="top"
           >
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
-              Content of first tab
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
-              Content of second tab
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
-              Content of third tab
-            </div>
-          </Tabs>
-        </StickyContainer>
+            <TabBar.Item
+              title="Life"
+              key="Life"
+              icon={<div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+              />
+              }
+              selectedIcon={<div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+              />
+              }
+              selected={this.state.selectedTab === 'blueTab'}
+              badge={1}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'blueTab',
+                });
+              }}
+              data-seed="logId"
+            >
+              {this.renderContent('Life')}
+            </TabBar.Item>
+            <TabBar.Item
+              icon={
+                <div style={{
+                  width: '22px',
+                  height: '22px',
+                  background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+                />
+              }
+              selectedIcon={
+                <div style={{
+                  width: '22px',
+                  height: '22px',
+                  background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+                />
+              }
+              title="Koubei"
+              key="Koubei"
+              badge={'new'}
+              selected={this.state.selectedTab === 'redTab'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'redTab',
+                });
+              }}
+              data-seed="logId1"
+            >
+              {this.renderContent('Koubei')}
+            </TabBar.Item>
+            <TabBar.Item
+              icon={
+                <div style={{
+                  width: '22px',
+                  height: '22px',
+                  background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+                />
+              }
+              selectedIcon={
+                <div style={{
+                  width: '22px',
+                  height: '22px',
+                  background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+                />
+              }
+              title="Friend"
+              key="Friend"
+              dot
+              selected={this.state.selectedTab === 'greenTab'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'greenTab',
+                });
+              }}
+            >
+              {this.renderContent('Friend')}
+            </TabBar.Item>
+            <TabBar.Item
+              icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+              selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+              title="My"
+              key="my"
+              selected={this.state.selectedTab === 'yellowTab'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'yellowTab',
+                });
+              }}
+            >
+              {this.renderContent('My')}
+            </TabBar.Item>
+          </TabBar>
+        </div>
         <WhiteSpace />
+        <div>
+          <SearchBar placeholder="Search" maxLength={8} />
+          <SearchBar placeholder="自动获取光标" ref={ref => this.autoFocusInst = ref} />
+        </div>
+        <WhiteSpace />
+        <div>
+          <WingBlank size="lg">
+            <Steps size="small" current={1}>
+              <Step title="Finished" description="This is description" />
+              <Step title="In Progress" description="This is description" />
+              <Step title="Waiting" description="This is description" />
+            </Steps>
+            <div className="sub-title">Small size, single line text</div>
+          </WingBlank>
+        </div>
+        <WhiteSpace />
+        <div>
+          <WingBlank>
+            <div className="loading-container">
+              <p className="sub-title">Without text</p>
+              <div className="loading-example">
+                <ActivityIndicator animating />
+              </div>
+            </div>
+          </WingBlank>
+        </div>
+        <WhiteSpace />
+
       </div>
     );
+  }
+  componentDidMount() {
+    this.autoFocusInst.focus();
+    clearTimeout(this.closeTimer);
   }
 }
 const mapStateToProps = (state) => {
